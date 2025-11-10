ed4edff9bd67fda93bae80b86ecde2a1a049f955
AnteroVehkaoja <apsoluutti@gmail.com>
2025-11-10 21:00:29 +0200

    pylint fixes of what know

--- week2/nhl-reader/src/index.py
+++ week2/nhl-reader/src/index.py
@@ -1,20 +1,18 @@
-import requests
-from player import Player,Player_reader, Player_Stats
+from player import PlayerReader, PlayerStats
 
 def main(nationality):
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    nhl = Player_reader(url)
+    nhl = PlayerReader(url)
     all_players = nhl.get_players()
-    
-    stats = Player_Stats(all_players)
+    stats = PlayerStats(all_players)
 
     nationality_players = stats.top_scores_by_nationality(str(nationality))
-    
+
     stats.show_players(nationality_players)
 
 
 
 
 if __name__ == "__main__":
-    nationality = input("Nationality: USA/FIN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS:  ")
-    main(nationality)
+    nationality2 = input("Nationality: USA/FIN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS:  ")
+    main(nationality2)
--- week2/nhl-reader/src/player.py
+++ week2/nhl-reader/src/player.py
@@ -4,16 +4,14 @@ from rich.table import Table
 
 
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict["nationality"]
-        self.assists = dict["assists"]
-        self.goals = dict["goals"]
-        self.team = dict["team"]
-        self.games = dict["games"]
-        self.id = dict["id"]
+    def __init__(self, dict1):
+        self.name = dict1['name']
+        self.nationality = dict1["nationality"]
+        self.assists = dict1["assists"]
+        self.goals = dict1["goals"]
+        self.team = dict1["team"]
+        self.games = dict1["games"]
         self.points = self.goals + self.assists
-    
     def __str__(self):
 
         table = Table(title="stuff")
@@ -25,42 +23,35 @@ class Player:
 
 
         console = Console()
-        return console.print(table)
-
-class Player_reader:
+        console.print(table)
+        return "how to fix"
+class PlayerReader:
     def __init__(self,url):
         self.response = requests.get(url).json()
-        
-        
     def get_players(self):
-
         players = []
-
         for player_dict in self.response:
             player = Player(player_dict)
             players.append(player)
         return players
 
-class Player_Stats:
+class PlayerStats:
     def __init__(self,players):
         self.players = players
 
 
     def top_scores_by_nationality(self,nationality):
 
-        self.nationality = nationality
 
         players = []
         for player in self.players:
-            if player.nationality == self.nationality:
+            if player.nationality == nationality:
                 players.append(player)
 
         players.sort(key=lambda happy:happy.goals + happy.assists, reverse= True)
-        return(players)
+        return players
 
     def show_players(self,players):
-
-
         table = Table(title="stuff")
 
         table.add_column("name", justify="right", style="cyan", no_wrap=True)
